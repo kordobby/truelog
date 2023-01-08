d370005ddf83bed7cbafe3442c1280e0166474a3
Yoon Lee <poordobby@naver.com>
2023-01-08 23:21:36 +0900

    feat. delete service

--- morethan-log.config.js
+++ morethan-log.config.js
@@ -1,35 +1,35 @@
 const CONFIG = {
   // profile setting (required)
   profile: {
-    name: 'leetrue',
-    image: '/leetrue.jpeg',  // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
-    role: 'frontend developer',
-    bio: 'I develop everything using node.',
-    email: 'poordobby@naver.com',
-    linkedin: '',
-    github: 'kordobby',
-    instagram: 'yuniscells',
+    name: "leetrue",
+    image: "/leetrue.jpeg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
+    role: "frontend developer",
+    bio: "I develop everything using node.",
+    email: "poordobby@naver.com",
+    linkedin: "",
+    github: "kordobby",
+    instagram: "yuniscells",
   },
   projects: [
     {
-      name: 'Untilled',
-      href: 'https://untilled.web.app'
-    }
+      // name: 'Untilled',
+      // href: 'https://untilled.web.app'
+    },
   ],
   // blog setting (required)
   blog: {
-    title: 'truelog',
-    description: 'welcome to truelog!',
-    theme: 'light' // ['light', 'dark', 'auto']
+    title: "truelog",
+    description: "welcome to truelog!",
+    theme: "light", // ['light', 'dark', 'auto']
   },
 
   // CONFIG configration (required)
-  link: 'https://morethan-log.vercel.app',
-  since: 2023, // If leave this empty, current year will be used.
-  lang: 'en-US', // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES', 'ko-KR']
-  ogImageGenerateURL: 'https://og-image-korean.vercel.app', // The link to generate OG image, don't end with a slash
+  link: "https://morethan-log.vercel.app",
+  since: 2022, // If leave this empty, current year will be used.
+  lang: "en-US", // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES', 'ko-KR']
+  ogImageGenerateURL: "https://og-image-korean.vercel.app", // The link to generate OG image, don't end with a slash
   seo: {
-    keywords: ['Blog', 'Website', 'Notion'],
+    keywords: ["Blog", "Website", "Notion"],
   },
 
   // notion configuration (required)
@@ -41,23 +41,23 @@ const CONFIG = {
   googleAnalytics: {
     enable: false,
     config: {
-      measurementId: process.env.GOOGLE_MEASUREMENT_ID || ''
-    }
+      measurementId: process.env.GOOGLE_MEASUREMENT_ID || "",
+    },
   },
   googleSearchConsole: {
     enable: false,
     config: {
-      siteVerification: process.env.GOOGLE_SITE_VERIFICATION || ''
-    }
+      siteVerification: process.env.GOOGLE_SITE_VERIFICATION || "",
+    },
   },
   utterances: {
     enable: true,
     config: {
-      repo: 'morethanmin/morethan-log',
-      'issue-term': 'og:title',
-      label: '💬 Utterances',
-    }
+      repo: "morethanmin/morethan-log",
+      "issue-term": "og:title",
+      label: "💬 Utterances",
+    },
   },
-  isProd: process.env.VERCEL_ENV === 'production' // distinguish between development and production environment (ref: https://vercel.com/docs/environment-variables#system-environment-variables)
-}
-module.exports = CONFIG
+  isProd: process.env.VERCEL_ENV === "production", // distinguish between development and production environment (ref: https://vercel.com/docs/environment-variables#system-environment-variables)
+};
+module.exports = CONFIG;
