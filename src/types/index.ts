323e5c3d6e3a3c2ff7b935111766431efa4e47fe
Yoon Lee <poordobby@naver.com>
2023-01-09 01:09:20 +0900

    init

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,27 +1,27 @@
 export type TPost = {
-  id: string
-  date: { start_date: string }
-  type: string[]
-  slug: string
-  tags?: string[]
-  summary?: string
+  id: string;
+  date: { start_date: string };
+  type: string[];
+  slug: string;
+  tags?: string[];
+  summary?: string;
   author?: {
-    id: string
-    first_name: string
-    last_name: string
-    profile_photo: string
-  }[]
-  title: string
-  status: string[]
-  createdTime: string
-  fullWidth: boolean
-  thumbnail?: string
-}
+    id: string;
+    first_name: string | null | undefined;
+    last_name: string | null | undefined;
+    profile_photo: string;
+  }[];
+  title: string;
+  status: string[];
+  createdTime: string;
+  fullWidth: boolean;
+  thumbnail?: string;
+};
 
-export type TPosts = TPost[]
+export type TPosts = TPost[];
 
 export type TTags = {
-  [tagName: string]: number
-}
+  [tagName: string]: number;
+};
 
-export type ThemeType = 'dark' | 'light'
+export type ThemeType = "dark" | "light";
