58640b077a22a2c60d3be3359fdb2e3d798a10b3
Yoon Lee <leetrue@chicment.com>
2023-05-07 21:17:05 +0900

    fixed bug

--- site.config.js
+++ site.config.js
@@ -34,7 +34,7 @@ const CONFIG = {
 
   // notion configuration (required)
   notionConfig: {
-    pageId: process.env.NEXT_PUBLIC_NOTION_PAGE_ID,
+    pageId: process.env.NOTION_PAGE_ID,
   },
 
   // plugin configuration (optional)
