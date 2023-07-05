447c653a1ee1c7b50bf34d1b9a299f0d772a180d
lxxtrue <poordobby@naver.com>
2023-07-05 22:47:26 +0900

    fix.

--- src/components/Layout/Header/index.tsx
+++ src/components/Layout/Header/index.tsx
@@ -5,7 +5,7 @@ import ThemeToggle from "./ThemeToggle"
 type Props = {
   fullWidth: boolean
 }
-
+// 프로그래머스
 const Header: React.FC<Props> = ({ fullWidth }) => {
   return (
     <div className="shadow-sm transition sticky bg-white dark:bg-zinc-700 mb-2 md:mb-6 border-b-[1px] z-[40] border-gray-100 dark:border-gray-800 top-0">
